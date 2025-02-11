76b6f07edeae703768f3ecaafc117d6159c4c09c
viveksapkal2793 <viveksapkal2003@gmail.com>
2025-02-11 20:43:18 +0530

    minor changes

--- game.py
+++ game.py
@@ -844,7 +844,6 @@ class Game:
             if self.player_on_rock is not None and player_obj:
                 # The rock moves downward => replicate the same shift
                 rock = self.player_on_rock
-                # player_obj.properties['position'][1] -= rock.properties['speed'] * delta
                 player_obj.properties['position'] = rock.properties['position']
                 
                 # Check if there's a key on this same rock
